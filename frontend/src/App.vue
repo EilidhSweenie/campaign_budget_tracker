05be224c51c4fd4cecf707cdf192b9f704904c3a
EilidhSweenie <EilidhSweenie@hotmail.com>
2025-11-15 16:32:19 +0000

    Add Campaign Table

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,191 @@
 <script setup>
+// Table items
+  const items = [
+    {
+      name: 'Account A',
+      budget: '£10000',
+      spend: '£50',
+      status: 'In Budget',
+    },
+    {
+      name: 'Account B',
+      budget: '£100',
+      spend: '£50',
+      status: 'Warning',
+    },
+    {
+      name: 'Account C',
+      budget: '£10',
+      spend: '£50',
+      status: 'Out of Budget',
+    },
+        {
+      name: 'Account A',
+      budget: '£10000',
+      spend: '£50',
+      status: 'In Budget',
+    },
+    {
+      name: 'Account B',
+      budget: '£100',
+      spend: '£50',
+      status: 'Warning',
+    },
+    {
+      name: 'Account C',
+      budget: '£10',
+      spend: '£50',
+      status: 'Out of Budget',
+    },
+        {
+      name: 'Account A',
+      budget: '£10000',
+      spend: '£50',
+      status: 'In Budget',
+    },
+    {
+      name: 'Account B',
+      budget: '£100',
+      spend: '£50',
+      status: 'Warning',
+    },
+    {
+      name: 'Account C',
+      budget: '£10',
+      spend: '£50',
+      status: 'Out of Budget',
+    },
+        {
+      name: 'Account A',
+      budget: '£10000',
+      spend: '£50',
+      status: 'In Budget',
+    },
+    {
+      name: 'Account B',
+      budget: '£100',
+      spend: '£50',
+      status: 'Warning',
+    },
+    {
+      name: 'Account C',
+      budget: '£10',
+      spend: '£50',
+      status: 'Out of Budget',
+    },
+        {
+      name: 'Account A',
+      budget: '£10000',
+      spend: '£50',
+      status: 'In Budget',
+    },
+    {
+      name: 'Account B',
+      budget: '£100',
+      spend: '£50',
+      status: 'Warning',
+    },
+    {
+      name: 'Account C',
+      budget: '£10',
+      spend: '£50',
+      status: 'Out of Budget',
+    },
+        {
+      name: 'Account A',
+      budget: '£10000',
+      spend: '£50',
+      status: 'In Budget',
+    },
+    {
+      name: 'Account B',
+      budget: '£100',
+      spend: '£50',
+      status: 'Warning',
+    },
+    {
+      name: 'Account C',
+      budget: '£10',
+      spend: '£50',
+      status: 'Out of Budget',
+    }
+  ]
+
+  // Define headers
+  const headers =[
+    { title: 'Name', key:'name', align:'start' },
+    { title: 'Budget', key:'budget' },
+    { title: 'Spend', key:'spend'  },
+    { title: 'Status', key:'status', align:'end', sortable: true }
+  ]
+
+  // Add new campaign button functionality 
+  const add = () =>{
+    console.log('Add campaign clicked')
+  }
+
+  // Return expected colour based on status
+  const getStatusColor = (status) => {
+  switch (status) {
+    case 'In Budget': return 'green'
+    case 'Warning': return 'orange'
+    case 'Out of Budget': return 'red'
+    default: return 'grey'
+  }
+}
 </script>
 
 <template>
-  <v-btn>Button</v-btn>
-</template>
+  <v-app>
+    <v-main>
+      <!-- Table Container -->
+      <v-container fluid>
+        <v-sheet border rounded>
+          <v-data-table 
+            :headers="headers"
+            :items="items"
+            >
+            <!-- Table header with add campaign button -->
+            <template v-slot:top>
+              <v-toolbar flat color="primary">
+                <!-- Title -->
+                <v-toolbar-title>
+                  Current Campaigns
+                </v-toolbar-title>
 
-<style scoped></style>
+                <!-- Button -->
+                <v-btn
+                  class="yellow-btn me-2"
+                  rounded="lg"
+                  prepend-icon="mdi-plus"
+                  border
+                  @click="add"
+                >
+                Add Campaign
+                </v-btn>
+              </v-toolbar>
+            </template>
+            
+            <!-- Render status on traffic light system based on status -->
+            <template v-slot:item.status="{ item }">
+              <v-chip :color="getStatusColor(item.status)">
+                {{ item.status }}
+              </v-chip>
+            </template>
+          </v-data-table>
+        </v-sheet>    
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+  
+
+<style>
+  .yellow-btn {
+    background-color: #FFDD33; 
+  }
+  .yellow-btn .v-btn__content,
+  .yellow-btn .v-icon {
+    color: #000 !important;
+  }
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,12 +6,30 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Icons
+import '@mdi/font/css/materialdesignicons.css'
+
 // Components
 import App from './App.vue'
 
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: 'myLightTheme',
+    themes: {
+      myLightTheme: {
+        dark: false,
+        colors: {
+          primary: '#FFFCEB',
+          secondary: '#FFDD33',  
+          surface: '#ffffff',
+          background: '#ffffff',
+          error: '#b00020',
+        },
+      },
+    },
+  },
 })
 
 createApp(App).use(vuetify).mount('#app')
